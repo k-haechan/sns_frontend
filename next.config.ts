a960414a3a73f13dc3e42804ff3b4c7a4e4c13d8
김해찬 <gibbm1127@naver.com>
2025-07-28 10:26:11 +0900

    fix: 불필요한 변수 정리

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import { URL } from "url";
-
-const storageBaseUrl = process.env.NEXT_PUBLIC_STORAGE_BASE_URL || "";
-const parsedStorageUrl = new URL(storageBaseUrl);
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // static.sns1.haechan.siteimages 도메인 추가
+      // static.sns1.haechan.site 도메인 추가
       {
         protocol: "https",
         hostname: "static.sns1.haechan.site",
